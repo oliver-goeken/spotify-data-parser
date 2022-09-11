7dd18e9be3683ee0db05bd7fbf8bb954e207ccba
oliver-goeken <bugseverywhere@protonmail.com>
2022-09-10 19:43:54 -0700

    more stuff

--- setup.py
+++ setup.py
@@ -4,15 +4,10 @@ setup(
     name="SpotifyDataParser",
     version="0.1.0",
     author="Oliver Goeken",
-    author_email="oliverdgoeken@gmail.com",
     packages=["spotifydataparser"],
-    scripts=["bin/stowe-towels.py", "bin/wash-towels.py"],
-    url="http://pypi.python.org/pypi/TowelStuff/",
+    url="https://github.com/oliver-goeken/spotify-data-parser",
     license="LICENSE.txt",
-    description="Useful towel-related stuff.",
+    description="Basic spotify listening data parsing.",
     long_description=open("README.txt").read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
+    install_requires=open("requirements.txt").read(),
 )
